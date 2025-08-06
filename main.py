fd1440fa96004f04c545954bd677de14d63fd4ca
carlbark <markus.carlbark@gmail.com>
2025-08-06 13:39:29 +0200

    Modified main() to accept the prompt as sys.argv (CLI parameter).

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 from dotenv import load_dotenv
 from google import genai
+import sys
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -10,9 +11,15 @@ client = genai.Client(api_key=api_key)
 def main():
     print("Hello from py-ai-agent!")
 
+    contents = sys.argv[1] if len(sys.argv) > 1 else None
+    if contents is None:
+        raise Exception("No contents provided. Please provide a string as an argument.")
+    else:
+        print(f"Contents: {contents}")
+
     response = client.models.generate_content(
         model="gemini-2.0-flash-001",
-        contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+        contents=contents
     )
     print("Response from Gemini API:")
     print(response.text)
